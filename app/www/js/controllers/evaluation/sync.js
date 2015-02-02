cd84b37b313ff1e472847fe3111e0086e95cbfa4
jiyee <jiyee.sheng@gmail.com>
2015-02-02 08:41:21 +0800

    增加同步单人选择

--- app/www/js/controllers/evaluation/sync.js
+++ app/www/js/controllers/evaluation/sync.js
@@ -1,12 +1,9 @@
 app.controller('EvaluationSyncCtrl', function($scope, $rootScope, $state, $stateParams, $ionicModal, $ionicPopup, $filter, settings, ProjectService, SegmentService, UserService, UnitService, CaptureService, CheckService, EvaluationService, OfflineService, AuthService, resolveUser) {
     $scope.data = {};
     $scope.data.user = resolveUser;
-    $scope.data.users = [];
-    $scope.data.users2 = [];
+    $scope.data.ranges = [];
     $scope.data.evaluation = {};
     $scope.data.evaluationId = $stateParams.evaluationId;
-    $scope.data.isCaptures = true;
-    $scope.data.isChecks = true;
 
     OfflineService.findById($scope.data.evaluationId).then(function(evaluation) {
         $scope.data.evaluation = evaluation;
@@ -14,44 +11,19 @@ app.controller('EvaluationSyncCtrl', function($scope, $rootScope, $state, $state
 
     UserService.findByUnitId($scope.data.user.unit._id).then(function(users) {
         $scope.users = _.clone(users, true);
-        $scope.data.users = [_.find($scope.users, {
-            name: $scope.data.user.name
-        })];
-        $scope.data.users[0].checked = true;
-        $scope.data.usernames = [$scope.data.user.name];
-
-        $scope.users2 = _.clone(users, true);
-        $scope.data.users2 = [_.find($scope.users2, {
-            name: $scope.data.user.name
-        })];
-        $scope.data.users2[0].checked = true;
-        $scope.data.usernames2 = [$scope.data.user.name];
     });
 
-    $ionicModal.fromTemplateUrl('users-modal.html', {
-        scope: $scope,
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
-    $scope.openModal = function($event) {
-        $scope.modal.show($event);
-    };
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-
-        $scope.data.users = [];
-        $scope.data.usernames = [];
-        _.each($scope.users, function(item) {
-            if (item.checked) {
-                $scope.data.usernames.push(item.name);
-                $scope.data.users.push(item);
-            }
-        });
-    };
-
-    $scope.openDatePicker = function(type) {
+    $scope.openDatePicker = function(item, type) {
         $scope.tmp = {};
         $scope.tmp.newDate = $scope.data[type];
+
+        if (!item.start_date) {
+            item.start_date = moment().subtract(1, 'month').format('YYYY-MM-DD HH:mm');
+        }
+
+        if (!item.end_date) {
+            item.end_date = moment().format('YYYY-MM-DD HH:mm');
+        }
 
         $ionicPopup.show({
             template: '<datetimepicker ng-model="tmp.newDate"></datetimepicker>',
@@ -63,90 +35,69 @@ app.controller('EvaluationSyncCtrl', function($scope, $rootScope, $state, $state
                 text: '<b>确定</b>',
                 type: 'button-positive',
                 onTap: function(e) {
-                    $scope.data[type] = $scope.tmp.newDate;
+                    item[type] = $scope.tmp.newDate;
+                    item['formatted_' + type] = $filter('date')($scope.tmp.newDate, 'yyyy-MM-dd HH:mm');
                 }
             }]
         });
     };
 
-    $scope.$watch('data.start_date', function(unformattedDate) {
-        $scope.data.formattedStartDate = $filter('date')(unformattedDate, 'yyyy-MM-dd HH:mm');
-    });
-
-    $scope.$watch('data.end_date', function(unformattedDate) {
-        $scope.data.formattedEndDate = $filter('date')(unformattedDate, 'yyyy-MM-dd HH:mm');
-    });
-
-    $scope.data.end_date = moment().format('YYYY-MM-DD HH:mm');
-    $scope.data.start_date = moment().subtract(1, 'month').format('YYYY-MM-DD HH:mm');
-
-    //////////////////
-    $ionicModal.fromTemplateUrl('users-modal2.html', {
-        scope: $scope,
-    }).then(function(modal) {
-        $scope.modal2 = modal;
-    });
-    $scope.openModal2 = function($event) {
-        $scope.modal2.show($event);
-    };
-    $scope.closeModal2 = function() {
-        $scope.modal2.hide();
-
-        $scope.data.users2 = [];
-        $scope.data.usernames2 = [];
-        _.each($scope.users2, function(item) {
-            if (item.checked) {
-                $scope.data.usernames2.push(item.name);
-                $scope.data.users2.push(item);
-            }
+    $scope.addCaptureRange = function () {
+        $scope.data.ranges.unshift({
+            type: "CAPTURE",
+            name: "安全检查",
+            user: "",
+            start_date: "",
+            end_date: ""
         });
     };
 
-    $scope.openDatePicker2 = function(type) {
-        $scope.tmp = {};
-        $scope.tmp.newDate2 = $scope.data[type];
-
-        $ionicPopup.show({
-            template: '<datetimepicker ng-model="tmp.newDate2"></datetimepicker>',
-            title: "日期选择",
-            scope: $scope,
-            buttons: [{
-                text: '取消'
-            }, {
-                text: '<b>确定</b>',
-                type: 'button-positive',
-                onTap: function(e) {
-                    $scope.data[type] = $scope.tmp.newDate2;
-                }
-            }]
+    $scope.addCheckRange = function () {
+        $scope.data.ranges.unshift({
+            type: "CHECK",
+            name: "日常巡检",
+            user: "",
+            start_date: "",
+            end_date: ""
         });
     };
-
-    $scope.$watch('data.start_date2', function(unformattedDate) {
-        $scope.data.formattedStartDate2 = $filter('date')(unformattedDate, 'yyyy-MM-dd HH:mm');
-    });
-
-    $scope.$watch('data.end_date2', function(unformattedDate) {
-        $scope.data.formattedEndDate2 = $filter('date')(unformattedDate, 'yyyy-MM-dd HH:mm');
-    });
-
-    $scope.data.end_date2 = moment().format('YYYY-MM-DD HH:mm');
-    $scope.data.start_date2 = moment().subtract(1, 'month').format('YYYY-MM-DD HH:mm');
-    //////////////////
 
     $scope.sync = function() {
-        if (_.isEmpty($scope.data.users) || _.isEmpty($scope.data.users2)) {
-            alert('请选择关联人员');
-            return;
-        }
+        var isValid = true;
+        var start_date, end_date;
 
-        if (!$scope.data.end_date || !$scope.data.end_date2) {
-            alert('请选择终止日期');
-            return;
-        }
+        _.each($scope.data.ranges, function (item) {
+            if (_.isEmpty(item.username)) {
+                alert('请选择关联人员');
+                isValid = false;
+                return;
+            }
 
-        if (!$scope.data.start_date || !$scope.data.start_date2) {
-            alert('请选择起始日期');
+            if (!item.end_date) {
+                alert('请选择终止日期');
+                isValid = false;
+                return;
+            }
+
+            if (!item.start_date) {
+                alert('请选择起始日期');
+                isValid = false;
+                return;
+            }
+
+            if (!start_date) {
+                start_date = item.start_date;
+            }
+
+            if (!end_date) {
+                end_date = item.end_date;
+            }
+
+            start_date = item.start_date < start_date ? item.start_date : start_date;
+            end_date = item.end_date > end_date ? item.end_date : end_date;
+        });
+
+        if (!isValid) {
             return;
         }
 
@@ -156,56 +107,45 @@ app.controller('EvaluationSyncCtrl', function($scope, $rootScope, $state, $state
 
         $rootScope.data.evaluation = $rootScope.data.evaluation || {};
         $rootScope.data.evaluation[$scope.data.evaluationId] = {};
-        $rootScope.data.evaluation[$scope.data.evaluationId].users = $scope.data.users;
+        // $rootScope.data.evaluation[$scope.data.evaluationId].users = $scope.data.users;
         $scope.$emit('sync', 'users');
 
-        if ($scope.data.isCaptures) {
-            CaptureService.list($scope.data.evaluation.project._id, $scope.data.evaluation.section._id, $scope.data.start_date, $scope.data.end_date).then(function(captures) {
-                $rootScope.data.evaluation[$scope.data.evaluationId].captures = _.filter(captures, function(capture) {
-                    return capture.process && capture.process.status !== 'END' && capture.user && _.find($scope.data.users, {
-                        name: capture.user.name
-                    }); // 过滤选择用户，且流程已走完，表示整改完成
-                });
-                $scope.$emit('sync', 'captures');
+        CaptureService.list($scope.data.evaluation.project._id, $scope.data.evaluation.section._id, start_date, end_date).then(function(captures) {
+            console.log(captures);
+            $rootScope.data.evaluation[$scope.data.evaluationId].captures = _.filter(captures, function(capture) {
+                return capture.process && capture.process.status !== 'END' && capture.user && _.find($scope.data.ranges, {
+                    type: "CAPTURE",
+                    username: capture.user.name
+                }); // 过滤选择用户，且流程已走完，表示整改完成
             });
-        } else {
             $scope.$emit('sync', 'captures');
-        }
+        });
 
-        if ($scope.data.isChecks) {
-            CheckService.list($scope.data.evaluation.project._id, $scope.data.evaluation.section._id, $scope.data.start_date2, $scope.data.end_date2).then(function(checks) {
-                $rootScope.data.evaluation[$scope.data.evaluationId].checks = _.filter(checks, function(check) {
-                    return check.process && check.process.status !== 'END' && check.user && _.find($scope.data.users2, {
-                        name: check.user.name
-                    });
+        CheckService.list($scope.data.evaluation.project._id, $scope.data.evaluation.section._id, start_date, end_date).then(function(checks) {
+            console.log(checks);
+            $rootScope.data.evaluation[$scope.data.evaluationId].checks = _.filter(checks, function(check) {
+                return check.process && check.process.status !== 'END' && check.user && _.find($scope.data.ranges, {
+                    type: "CHECK",
+                    username: check.user.name
                 });
-                $scope.$emit('sync', 'checks');
             });
-        } else {
             $scope.$emit('sync', 'checks');
-        }
+        });
     };
 
     $scope.$on('sync', function(evt, type) {
         $scope.data.sync = _.without($scope.data.sync, type);
 
         if (_.isEmpty($scope.data.sync)) {
-            if ($scope.data.isChecks || $scope.data.isCaptures) {
-                ActivityIndicator.hide();
-                alert('同步成功');
-            }
+            console.log($rootScope.data.evaluation[$scope.data.evaluationId]);
+            ActivityIndicator.hide();
+            alert('同步成功');
 
             $state.go('^.customize', {
                 evaluationId: $scope.data.evaluationId
             });
         }
     });
-
-    $scope.skip = function() {
-        $state.go('^.customize', {
-            evaluationId: $scope.data.evaluationId
-        });
-    };
 
     $scope.toBack = function() {
         $state.go('^.list');
